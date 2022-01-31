59d0abca60719fd5b850a823f7e5c4fb106f3304
mounicakadaagunta <mounicakadapagunta13@gmail.com>
2022-01-31 21:16:52 +0530

    mongodb url updated

--- config/configuration.js
+++ config/configuration.js
@@ -1,5 +1,5 @@
 module.exports = {
-    mongoDbUrl: 'mongodb+srv://gautham:gautham@blog.jcckq.mongodb.net',
+    mongoDbUrl: 'mongodb+srv://gautham:gautham@blog.jcckq.mongodb.net/cms_app',
     // mongoDbUrl: 'mongodb://localhost:27017/cms_app',
     PORT: process.env.PORT || 3002,
     //this willl act as middleware and we will use all over our project
